a4abc14f4009c098388bef0faa4880513b5800e3
Mohamed Mahmoud <mohamed.mahmoud.abuelmaati@gmail.com>
2022-12-01 18:41:49 +0200

    Edit Modal Button Text

--- src/components/Modal/ModalFooter.tsx
+++ src/components/Modal/ModalFooter.tsx
@@ -27,7 +27,7 @@ const ModalFooter: FC<{
           onClick={() => onHandleSubmit()}
           className="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
         >
-          I accept
+          Create
         </button>
       </div>
     </div>
